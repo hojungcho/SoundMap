2c47c32771d63067646083931ab5ddf99546c91c
Seojin <xeojine@gmail.com>
2024-10-20 11:24:20 +0900

    recommendation_tile 레이아웃 수정

--- sound_map/lib/UI/second_ui.dart
+++ sound_map/lib/UI/second_ui.dart
@@ -31,10 +31,41 @@ class SecondUi extends StatelessWidget {
                 ),
               ),
 
-              // song recommendation tiles - feature filter
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: VideoList_1(),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Feature List',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0
+                        ),
+                      ),
+                    ),
+
+                    // song recommendation tiles - feature filter
+                    VideoList_1(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Basic List',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                ),
               ),
             ],
           ),
@@ -42,7 +73,7 @@ class SecondUi extends StatelessWidget {
 
         // song recommendation tiles - default
         Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
           child: SizedBox(
             width: MediaQuery.of(context).size.width,
             child: VideoList_2(),
