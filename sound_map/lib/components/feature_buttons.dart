c61c4ec7346ddab9c76c6d2be31ffa81d4796ee4
Seojin <xeojine@gmail.com>
2024-10-21 09:27:00 +0900

    feature button scrollable 기능 추가

--- sound_map/lib/components/feature_buttons.dart
+++ sound_map/lib/components/feature_buttons.dart
@@ -19,33 +19,37 @@ class _FeatureSelectorState extends State<FeatureButtons> {
       padding: const EdgeInsets.only(left: 10.0, right: 10.0),
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.4,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (var i=0; i<feature.length; i++)
-              InkWell(
-                onTap: (){
-                  setState(() {
-                    selected_feauture.contains(feature[i]) ? selected_feauture.remove(feature[i]) : selected_feauture.add(feature[i]);
-                  });
-                },
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  padding: EdgeInsets.symmetric(horizontal:  10, vertical: 5),
-                  decoration: BoxDecoration(
-                    color: selected_feauture.contains(feature[i]) ? feature_colors[i] : Colors.white,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Text(
-                    feature[i],
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: selected_feauture.contains(feature[i]) ? Colors.white : Colors.black
+        child: Center(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for (var i=0; i<feature.length; i++)
+                  InkWell(
+                    onTap: (){
+                      setState(() {
+                        selected_feauture.contains(feature[i]) ? selected_feauture.remove(feature[i]) : selected_feauture.add(feature[i]);
+                      });
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.symmetric(horizontal:  10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: selected_feauture.contains(feature[i]) ? feature_colors[i] : Colors.white,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        feature[i],
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: selected_feauture.contains(feature[i]) ? Colors.white : Colors.black
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              )
-          ],
+                  )
+              ],
+            ),
+          ),
         ),
       ),
     );
